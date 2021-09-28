b5f6c3b20b0614b0f0026265e9182177babb9fab
daherre0 <daherre0@gmail.com>
2021-09-28 08:00:59 -0500

    Tengo un error, no sé qué hacer

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hola grupo 53')
+numero = input('Pasa el número: ')
+
+cont = 0
+for i in range(numero):
+    if numero % i == 0:
+        cont = cont  +1
+
+if cont == 2:
+    print('el número ', numero, ' es Primo.')
+else:
+    print('el número ', numero, ' No es primo.')
